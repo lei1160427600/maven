911257bd2832843abc0e6af814a5a99db8ab8ce9
xiaolei <xuechao_lei@126.com>
2017-01-09 23:51:56 +0800

    提交文件

---
+++ src/main/java/com/maven/Hello.java
@@ -0,0 +1,10 @@
+package com.maven;
+public class Hello{
+	public String sayHello(String name) {
+
+		return "Hello " + name;
+	}
+	public static void main(String[] arg){
+		System.out.print("Hello ");
+	}
+}
---
+++ src/test/java/com/maven/Tests.java
@@ -0,0 +1,14 @@
+package com.maven;
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Tests {
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testHello(){
+		Hello hello = new Hello();
+		String results = hello.sayHello("maven");
+		assertEquals("Hello maven", results);
+	}
+}
