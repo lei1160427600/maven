86b7ffa381c56112e1004da53a339304e3541bd6
xiaolei <xuechao_lei@126.com>
2017-01-10 00:45:39 +0800

    change

--- src/main/java/com/maven/Hello.java
+++ src/main/java/com/maven/Hello.java
@@ -2,7 +2,7 @@ package com.maven;
 public class Hello{
 	public String sayHello(String name) {
 
-		return "Hello " + name;
+		return "Hello " + name + " !";
 	}
 	public static void main(String[] arg){
 		System.out.print("Hello ");
